a2c295f7425848f77fc193f5a2aaa683e3f627a8
AlvaroCS <alvaro.corral@innocv.com>
2019-04-03 13:25:03 +0200

    Iteration 2

--- starter-code/src/app/contact-list/contact-list.component.ts
+++ starter-code/src/app/contact-list/contact-list.component.ts
@@ -19,6 +19,9 @@ export class ContactListComponent implements OnInit {
   addContact(){
     console.log("Add contact has been called");
     // add contact to contacts list
+    this.contacts.push(this.newContact)
+    console.log(this.contacts)
     // clear inputs
+    this.newContact={}
   }
 }
